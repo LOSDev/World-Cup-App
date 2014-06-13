e94ef6a60660e6766783eea1aa11aa0bad0c4fe3
rincewind <rincewind@rincewind-VirtualBox.(none)>
2014-06-13 13:40:03 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Worldcup::Application.config.secret_key_base = '41446a5d72dba8942b8fb62bd10ee0f6f39b33891d690fad43e315bc894d4d274fcb53591d93d4eb223063eb644cfb0c2560df3305690a530edc704080c89400'
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Group.create(name: "A")
+Group.create(name: "B")
+Group.create(name: "C")
+Group.create(name: "D")
+Group.create(name: "E")
+Group.create(name: "F")
+Group.create(name: "G")
+Group.create(name: "H")
